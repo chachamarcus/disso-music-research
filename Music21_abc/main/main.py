0daddda9be3037f382bb692fdd3e79d1ca42b1ec
Charlie Marcus <chachamarcus@googlemail.com>
2018-02-14 15:43:36 +0000

    find all chords in stream

--- Music21_abc/main/main.py
+++ Music21_abc/main/main.py
@@ -1,19 +1,29 @@
 import argparse
-from music21 import converter
+from music21 import converter, analysis, stream
 import tkinter as tk;
 from tkinter import filedialog
+import sys
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-key', help='run Krumhansl algorithm on an abc file', action="store_true")
+    parser.add_argument('-key', help='run Krumhansl algorithm on a music file', action="store_true")
+    parser.add_argument('-rna', help='run roman numeral analysis a music file', action="store_true")
     args = parser.parse_args()
     
     root = tk.Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename()
+    score = converter.parse(file_path)
 
-    if (args.key):
-        score = converter.parse(file_path)
-        print(score)
+    if args.key or args.rna:
+        key = analysis.discrete.analyzeStream(score, 'Krumhansl')
+        sys.stdout.write(str(key) + '\n')
+    
+    if args.rna:
+        chordFilter = stream.filters.ClassFilter('Chord')
+        sIter = score.recurse().iter
+        sIter.addFilter(chordFilter)
+        for a in sIter:
+            sys.stdout.write(str(a) + '\n')
     
 if __name__ == "__main__": main()
